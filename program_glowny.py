e671f4071a1221e8b4339ab1da991030f466011f
Wiktor Kycia <wiktor.kycia@gmail.com>
2022-01-23 13:17:38 +0100

    wprowadzenie listy liczby, zamiast pliku

--- program_glowny.py
+++ program_glowny.py
@@ -2,9 +2,9 @@ lista_znakow = ["+", "-", "*", "/", "="]
 lista_liczb = ['1','2','3','4','5','6','7','8','9','0']
 wyraz = input("Podaj wyrażenie: ")
 a=0
-działanie = 0
 wynik = None
 znaki = []
+liczby = []
 
 def dodaj(a,b):
     return a+b
@@ -25,18 +25,19 @@ for i, elem in enumerate(wyraz):
 
     if elem in lista_liczb:
         elem = int(elem)
-        if działanie == 0:
-            if a == 0:
-                a = elem
-            elif a != 0:
-                a = 10*a + elem
-            plik = open("liczby","a+")
-            a = str(a)
-            plik.write(a)
-            plik.close()
-            a = int(a)
-            wynik = a
-        
+        if a == 0:
+            a = elem
+        elif a != 0:
+            a = 10 * a + elem
+        plik = open("liczby", "a+")
+        a = str(a)
+        plik.write(a)
+        plik.write("\n")
+        plik.close()
+        a = int(a)
+        wynik = a
+
+
     else:
         print("Wystąpił bład")
         print("Prawdopodownie wpisałeś literę a nie liczbę")
